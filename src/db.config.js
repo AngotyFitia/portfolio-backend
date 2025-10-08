3f3d0d0716c496733316242a3d2950494304f774
Angoty <angotyrabarijaona@gmail.com>
2025-10-08 22:39:17 +0300

    test-database-online

--- src/db.config.js
+++ src/db.config.js
@@ -1,16 +1,15 @@
 import dotenv from 'dotenv';
 import pkg from 'pg';
-const { Pool } = pkg;
 
+const { Pool } = pkg;
 
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // requis pour Neon
+  },
 });
 
 export default pool;
